331d3c35139d20e889c8818eb3f27d8cc64b818f
Nicolas <cathyannick@hotmail.fr>
2020-09-08 11:36:17 +0200

    ajout image

--- app.js
+++ app.js
@@ -2,30 +2,37 @@ const express = require('express')
 const mysql = require('mysql')
 const methodOverride = require('method-override')
 const path = require('path')
+const fileUpload = require('express-fileupload');
 const util = require('util')
 //const { query } = require('express')
 const port = 7654
 const app = express()
+
+app.use(fileUpload());
 
 require('dotenv').config()
 
 app.set('view engine', 'ejs');
 
 app.use(express.json())
-app.use(express.urlencoded({extended:false}))
+app.use(express.urlencoded({
+    extended: false
+}))
 
-const db = mysql.createConnection ({    
+const db = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_DATABASE,
-   // multipleStatements: true
+    // multipleStatements: true
 });
 
 
 
-  db.connect((err) => {
-    if (err) { throw err;}
+db.connect((err) => {
+    if (err) {
+        throw err;
+    }
     console.log('Connecté à la base MySQL');
 });
 
@@ -36,12 +43,12 @@ global.query = query
 //app.set('views', path.join(__dirname, 'views'));
 app.use(methodOverride('_method'))
 
-app.use(express.static(path.join(__dirname,'./public')));
+app.use(express.static(path.join(__dirname, './public')));
 // CONTROLLER
 
 // ROUTES
-app.get('/', async(req, res)=>{
-     //let query = [
+app.get('/', async (req, res) => {
+    //let query = [
     //     "SELECT * FROM factories",
     //     "SELECT * FROM energies"  
     // ]
@@ -49,7 +56,10 @@ app.get('/', async(req, res)=>{
     const factories = await query("SELECT * FROM factories")
     const energies = await query("SELECT * FROM energies")
 
-    res.render('index',{factories, energies})
+    res.render('index', {
+        factories,
+        energies
+    })
 
     // db.query(query,(err, result)=>{
     //     if (err){
@@ -61,35 +71,61 @@ app.get('/', async(req, res)=>{
     // })
 
 
-    
+
 })
-app.get('/table/add', (req, res)=>{
+app.get('/table/add', (req, res) => {
     res.render("addintables")
 })
-app.post('/table/add/factory', async(req, res)=>{
-    //let factoryId = req.body.factoryId
-    let factoryName = req.body.factoryName
-   // let energyId = req.body.energyId
-     let energyName = req.body.energyName
-    if(factoryName.length || energyName.length){
-          try {
-        await query('insert into factories (name) values(?);',[factoryName]);
-        await query('insert into energies (name) values(?);', [energyName]);
-        res.redirect('/');
-        } catch (err){
-        res.send(err) 
-        } 
-    }else {
-        res.send('bug')
+app.post('/table/add/factory', (req, res) => {
+    if (!req.files || Object.keys(req.files).length === 0) {
+        return res.status(400).send('No files were uploaded.');
     }
-       
-    
-      
+    //let factoryId = req.body.factoryId
+    let name = req.body.name
+    // let energyId = req.body.energyId
+    let energyName = req.body.energyName
+    let imageUpload = req.files.image
+    let image = `public/images/${imageUpload.name}`
+    //if(factoryName.length || energyName.length){
+
+    if (imageUpload.mimetype === "image/jpeg" || imageUpload.mimetype === "image/jpg" || imageUpload.mimetype === "image/gif" || imageUpload.mimetype === "image/png") {
+
+        imageUpload.mv(`public/images/${imageUpload.name}`, async function (err) {
+                if (err) {
+                    return res.status(500).send(err);
+                }
+                try {
+                    await query('INSERT INTO cars_table (name, image) VALUES (?,?);', [name, image])
+                    res.redirect('/');
+                } catch (err) {
+                    res.send(err)
+                }
+
+                //res.send('File uploaded!');
+            }
+
+
+        );
+    } else {
+        message = "fichier invalide"
+        res.render('addintables', {
+            message
+        })
+    }
+    //   console.log(imageUpload);
+    //await query('insert into factories (name) values(?);',[factoryName]);
+    //await query('insert into energies (name) values(?);', [energyName]);
+    //res.send("ok")
+
+    // await query('insert into cars (name) values(?);', [energyName]); 
+    //}else {
+    //   res.send('bug')
+    // }
 })
 
-   
+
 // app.delete('/table/delete',(req, res)=>{
 //     let id = req.param.id
-    
+
 // })
-  app.listen(port, console.log(`localhost run on port:${port}`))
+app.listen(port, console.log(`localhost run on port:${port}`))
